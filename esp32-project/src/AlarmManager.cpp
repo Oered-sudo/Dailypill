10de83bb7c1e788d759754a1660e31c572a0bb2b
Oered-sudo <178186288+Oered-sudo@users.noreply.github.com>
2025-04-29 08:57:58 +0000

    Refactor AlarmManager et Display : réorganisation des structures de données et amélioration de la gestion des alarmes

--- esp32-project/src/AlarmManager.cpp
+++ esp32-project/src/AlarmManager.cpp
@@ -1,14 +1,54 @@
 #include "AlarmManager.h"
+#include <Arduino.h>
+#include <vector>
+#include <string>
 
-void AlarmManager::addAlarm(const String& name, int hour, int minute) {
-    alarms.push_back({name, hour, minute});
-}
+struct Alarm {
+    String name;
+    int hour;
+    int minute;
+    bool active;
+};
 
-bool AlarmManager::isAlarmActive() {
-    // Vérifiez si une alarme est active (ajoutez votre logique ici)
-    return false;
-}
+class AlarmManager {
+private:
+    std::vector<Alarm> alarms;
 
-const std::vector<Alarm>& AlarmManager::getAlarms() const {
-    return alarms;
-}
+public:
+    void addAlarm(const String& name, int hour, int minute) {
+        Alarm newAlarm = { name, hour, minute, true };
+        alarms.push_back(newAlarm);
+    }
+
+    void removeAlarm(int index) {
+        if (index >= 0 && index < alarms.size()) {
+            alarms.erase(alarms.begin() + index);
+        }
+    }
+
+    void updateAlarm(int index, const String& name, int hour, int minute) {
+        if (index >= 0 && index < alarms.size()) {
+            alarms[index].name = name;
+            alarms[index].hour = hour;
+            alarms[index].minute = minute;
+        }
+    }
+
+    void checkAlarms() {
+        int currentHour = hour();
+        int currentMinute = minute();
+        for (const auto& alarm : alarms) {
+            if (alarm.active && alarm.hour == currentHour && alarm.minute == currentMinute) {
+                triggerAlarm(alarm);
+            }
+        }
+    }
+
+    void triggerAlarm(const Alarm& alarm) {
+        // Code to trigger the alarm (e.g., sound a buzzer, send a notification)
+    }
+
+    const std::vector<Alarm>& getAlarms() const {
+        return alarms;
+    }
+};
